3b6c1bec79e93f726177724928fe59594c5c0030
Andrew <andrewhtjoseph@gmail.com>
2019-09-13 09:07:34 -0700

    Create simpleioquiz.c

---
+++ simpleioquiz.c
@@ -0,0 +1,35 @@
+//
+//  main.c
+//  Tutorial 2
+//
+//  Created by Joseph, Andrew on 9/3/19.
+//  Copyright © 2019 Joseph, Andrew. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Simple.h"
+#include "Q2.h"
+
+
+int main(int argc, const char * argv[]) {
+    int score = 0;
+
+printf("Welcome to the Simple I/O Quiz!");
+ 
+    if (simpleio1(5) == 1){
+        score++;
+    }
+
+    if (question2(5) ==1){
+        score++;
+    }
+    printf("%d out of 2\n", score);
+    printf("%d percent\n",score*100/2 );
+
+
+    
+return 0;
+}
+
+
+          
