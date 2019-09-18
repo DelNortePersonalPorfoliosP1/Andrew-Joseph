97bab1cf4095c9c7e06a722e6d7675413441d817
John Mortensen <jmortensen@powayusd.com>
2019-09-18 16:43:26 -0700

    missing files

---
+++ Portfolio/quiz.c
@@ -0,0 +1,12 @@
+//
+//  quiz.c
+//  Tutorial 2
+//
+//  Created by Joseph, Andrew on 9/3/19.
+//  Copyright © 2019 Joseph, Andrew. All rights reserved.
+//
+#include "quiz.h"
+void quizmain()
+{   printf("quiz\n");
+    return;
+}
---
+++ Portfolio/time.c
@@ -0,0 +1,12 @@
+//
+//  quiz.c
+//  Tutorial 2
+//
+//  Created by Joseph, Andrew on 9/3/19.
+//  Copyright © 2019 Joseph, Andrew. All rights reserved.
+//
+#include "time.h"
+void timer()
+{   printf("timer\n");
+    return;
+}
